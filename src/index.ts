0c14171c885d483ba355de6ef7b96da683fac51a
princejoogie <princejoogie@gmail.com>
2022-11-10 12:11:26 +0800

    chore: add startBot
    modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { getVersion } from "./util";
 
 const PORT = process.env["PORT"] ?? 4000;
 
-const main = async () => {
+const startServer = async () => {
   const app = express();
 
   app.use(cors());
@@ -17,6 +17,10 @@ const main = async () => {
   app.use(express.static("public"));
   app.use(morgan("combined"));
 
+  app.get("/", (_, res) => {
+    return res.send(`eigooj bot v${getVersion()}`);
+  });
+
   app.listen(PORT, () => {
     console.log(
       `Server (v${getVersion()}) listening on http://localhost:${PORT}`
@@ -24,7 +28,17 @@ const main = async () => {
   });
 };
 
-main().catch(() => {
-  console.error("An error occurred");
-  process.exit(1);
-});
+const startBot = async () => {
+  console.log("Starting bot...");
+};
+
+const main = async () => {
+  try {
+    await Promise.all([startServer(), startBot()]);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+main();
